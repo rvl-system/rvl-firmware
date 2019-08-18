43a16821d7f3096ecc1471a696e2e18e08dd93a5
Bryan Hughes <bryan@nebri.us>
2019-08-18 00:30:01 -0700

    Added base station code (again)

--- firmware/src/state.cpp
+++ firmware/src/state.cpp
@@ -25,7 +25,7 @@ along with Raver Lights.  If not, see <http://www.gnu.org/licenses/>.
 namespace State {
 
 #define SUNRISE_HOUR 10
-#define SUNRISE_MINUTE 41
+#define SUNRISE_MINUTE 0
 
 #define SUNSET_HOUR 20
 #define SUNSET_MINUTE 0
